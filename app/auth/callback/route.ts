d73358459ab093027bf1048c71cf6441de14a3d9
mattej5 <matthew.jones6288@gmail.com>
2025-10-05 17:30:43 -0600

    Updated auth

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -17,8 +17,8 @@ export async function GET(request: Request) {
   const cookieStore = await cookies();
 
   const supabase = createServerClient(
-    process.env.SUPABASE_URL!,            // use server-only envs
-    process.env.SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() {
